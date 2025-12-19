04a8a48e618d8b571b9ba3af9ad6a133208c2c44
mohamed <ibs.developer1@gmail.com>
2025-12-19 18:39:13 +0200

    asf

--- odex25_inventory/add_menu_exp_account/__manifest__.py
+++ odex25_inventory/add_menu_exp_account/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'mail', 'purchase','web', 'message'],
+    'depends': ['base', 'account', 'mail', 'purchase','web'],
 
     # always loaded
     'data': [
